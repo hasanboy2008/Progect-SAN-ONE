5b6682ee1909a719cf824298a3dd01b48ba4f5ab
Jamshidbeck <jamshidabdullajonov@gmail.com>
2022-07-07 22:27:24 +0500

    Basket pageni qo'shdim

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,6 +8,7 @@ import "./main.css"
 import { Futter } from "../Futter/Futer";
 import { Confirm } from "../../pages/Confirm/Confirm";
 import { Detail } from "../../pages/Detail/Detail";
+import { Basket } from "../../pages/Basket/Basket";
 
 
 
@@ -26,6 +27,7 @@ export function Main(){
            */}
           <Route path="/Confirm" element={<Confirm />} />
           <Route path="/Detail" element={<Detail />} />
+          <Route path="/Basket" element={<Basket />} />
         </Routes>
         <Futter />
       </div>
