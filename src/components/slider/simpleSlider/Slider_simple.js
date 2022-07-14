a173b9769a2805838ce1ad38b7c84263988d3980
bekicom <adelkauz6@gmail.com>
2022-07-15 03:05:50 +0500

    navbar ful boley dedi

--- src/components/slider/simpleSlider/Slider_simple.js
+++ src/components/slider/simpleSlider/Slider_simple.js
@@ -66,7 +66,7 @@ export default function Simple_slider() {
                 >
                   Sotib olish
                 </button>
-                <button>Savatga</button>
+             
               </div>
             </div>
           ))}
