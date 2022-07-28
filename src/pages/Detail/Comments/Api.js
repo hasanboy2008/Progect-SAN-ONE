56a13a83a77dc97bbaea6d829d3b6f0b749dab5f
Muhammadsaid2oo6 <muhammadsaid06hasanboyev@gmail.com>
2022-07-28 15:34:51 +0500

    comment

--- src/pages/Detail/Comments/Api.js
+++ src/pages/Detail/Comments/Api.js
@@ -1,37 +1,6 @@
 export const getComments = async () => {
     return [
-      {
-        id: "1",
-        body: "Bu poyabzal vapshe Jinni! Zo`r ekan! 1 kunda yetib keldi Sotib olishni tavsiya qilaman",
-        username: "Abdukarimov Zohidbek",
-        userId: "1",
-        parentId: null,
-        createdAt: "2021-08-16T23:00:33.010+02:00",
-      },
-      {
-        id: "2",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to",
-        username: "Hasanov Axrorx",
-        userId: "2",
-        parentId: null,
-        createdAt: "2022-08-16T23:00:33.010+02:00",
-      },
-      {
-        id: "3",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to",
-        username: "Abdukarimov Zohidbek",
-        userId: "2",
-        parentId: "1",
-        createdAt: "2021-08-16T23:00:33.010+02:00",
-      },
-      {
-        id: "4",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to",
-        username: "Abdukarimov Zohidbek",
-        userId: "2",
-        parentId: "2",
-        createdAt: "2021-08-16T23:00:33.010+02:00",
-      },
+      
     ];
   };
   
