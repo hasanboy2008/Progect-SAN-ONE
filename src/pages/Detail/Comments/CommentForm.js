19d5eda6f6031bbab9122494148ef4368c86ecbf
Muhammadsaid2oo6 <muhammadsaid06hasanboyev@gmail.com>
2022-07-28 20:34:49 +0500

    dsd

--- src/pages/Detail/Comments/CommentForm.js
+++ src/pages/Detail/Comments/CommentForm.js
@@ -1,37 +1,54 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 
-import tg from "../../../asest/Detail/Group 8557.png"
+import tg from "../../../asest/Detail/Group 8557.png";
 
-const CommentForm = ({
-  handleSubmit,
-  submitLabel,
-  hasCancelButton = false,
-  handleCancel,
-  initialText = "",
-}) => {
+const CommentForm = ({ handleSubmit, submitLabel, initialText = "" }) => {
+  const location = useLocation();
+  const product_id = location.search.split("=")[1];
+
   const [text, setText] = useState(initialText);
+  const user = useSelector((state) => state.reUser);
   const isTextareaDisabled = text.length === 0;
   const onSubmit = (event) => {
     event.preventDefault();
     handleSubmit(text);
     setText("");
+    axios("https://api.sanone.uz/addComments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        product_id: product_id,
+        userId: user.id,
+        comment: text,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
+
   return (
-    <form onSubmit={onSubmit} >
+    <form onSubmit={onSubmit}>
       <div className="textArea">
         <textarea
           placeholder="Izoh yozing"
           className="comment-form-textarea"
           value={text}
           onChange={(e) => setText(e.target.value)}
-
         />
 
         <button className="comment-form-button" disabled={isTextareaDisabled}>
           {submitLabel} <img src={tg} alt="" />
         </button>
       </div>
-
 
       <hr />
 
--- src/pages/Detail/Comments/Comments.js
+++ src/pages/Detail/Comments/Comments.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import CommentForm from "./CommentForm";
+import { useLocation } from "react-router-dom";
+
 import Comment from "./Comment";
 import {
   getComments as getCommentsApi,
@@ -7,8 +9,10 @@ import {
   updateComment as updateCommentApi,
   deleteComment as deleteCommentApi,
 } from "./Api";
+import axios from "axios";
 
 const Comments = ({ commentsUrl, currentUserId }) => {
+  const [comments, setComments ] = useState([])
   const [backendComments, setBackendComments] = useState([]);
   const [activeComment, setActiveComment] = useState(null);
   const rootComments = backendComments.filter(
@@ -41,7 +45,9 @@ const Comments = ({ commentsUrl, currentUserId }) => {
     });
   };
   const deleteComment = (commentId) => {
-    if (window.confirm("Haqiqatdan ham commentariyangizni o'chirmoqchimisiz?")) {
+    if (
+      window.confirm("Haqiqatdan ham commentariyangizni o'chirmoqchimisiz?")
+    ) {
       deleteCommentApi().then(() => {
         const updatedBackendComments = backendComments.filter(
           (backendComment) => backendComment.id !== commentId
@@ -56,12 +62,23 @@ const Comments = ({ commentsUrl, currentUserId }) => {
       setBackendComments(data);
     });
   }, []);
+  const location = useLocation();
+  const product_id = location.search.split("=")[1];
+  useEffect(() => {
+    axios(`https://api.sanone.uz/viewComments/${product_id}`)
+      .then((res) => {
+        setComments(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className="comments">
-      <CommentForm submitLabel="" handleSubmit={addComment}  />
+      <CommentForm submitLabel="" handleSubmit={addComment} />
       <div className="comments-container">
-        {rootComments.map((rootComment) => (
+        {comments.map((rootComment) => (
           <Comment
             key={rootComment.id}
             comment={rootComment}
